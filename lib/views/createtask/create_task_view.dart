8e7834eeedf313f6c72e51ebfe4cb0f9fb11e89e
meouz <samuel.deanova@gmail.com>
2025-01-28 02:03:54 +0700

    feat: Add create task

--- lib/views/createtask/create_task_view.dart
+++ lib/views/createtask/create_task_view.dart
@@ -10,6 +10,11 @@ class CreateTaskView extends StatefulWidget {
 }
 
 class _CreateTaskViewState extends State<CreateTaskView> {
+  String title = '';
+  String description = '';
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedTime = TimeOfDay.now();
+
   @override
   Widget build(BuildContext context) {
     return Consumer(
@@ -17,21 +22,62 @@ class _CreateTaskViewState extends State<CreateTaskView> {
               appBar: AppBar(
                 title: const Text('Create Task'),
               ),
-              body: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    final taskModel = context.read<TaskService>();
-                    // Adding a task with dummy data
-                    taskModel.addTask(
-                      TaskServiceItem(
-                        title: 'Dummy Task',
-                        description: 'This is a dummy task',
-                        dueDate: DateTime.now().toString()
-                      ),
-                    );
-                  },
-                  child: const Text('Create Dummy Task'),
-                ),
+              body: Column(
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      title = value;
+                    },
+                    decoration: const InputDecoration(hintText: 'Title'),
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      description = value;
+                    },
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        final DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: _selectedDate,
+                          firstDate: DateTime(2021),
+                          lastDate: DateTime(DateTime.now().year + 5),
+                        );
+                        if (pickedDate != null && pickedDate != _selectedDate) {
+                          setState(() {
+                            _selectedDate = pickedDate;
+                          });
+                        }
+                      },
+                      child: Text(
+                          'Due Date: ${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}')),
+                  ElevatedButton(
+                      onPressed: () async {
+                        final TimeOfDay? pickedTime = await showTimePicker(
+                            context: context, initialTime: TimeOfDay.now());
+                        if (pickedTime != null && pickedTime != _selectedTime) {
+                          setState(() {
+                            _selectedTime = pickedTime;
+                          });
+                        }
+                      },
+                      child: Text('Time: ${_selectedTime.format(context)}')),
+                  ElevatedButton(
+                    onPressed: () {
+                      final taskModel = context.read<TaskService>();
+                      taskModel.addTask(
+                        TaskServiceItem(
+                          title: title,
+                          description: description,
+                          dueDate:
+                              '${_selectedDate.year}/${_selectedDate.month}/${_selectedDate.day} ${_selectedTime.format(context)}',
+                        ),
+                      );
+                    },
+                    child: const Text('Create Task'),
+                  ),
+                ],
               ),
             ));
   }
